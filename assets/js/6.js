dcc637b91ef9244d259b7ce17f9ad91673728983
romaingrtrd <84959002+romaingrtrd@users.noreply.github.com>
2022-11-17 17:35:43 +0100

    enigme6

---
+++ assets/js/6.js
@@ -0,0 +1,132 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const taskDone = [
+    'Rentrer dans les bâtiments de G4',
+    'Phase de connexion',
+    'Planification de la semaine',
+    'Prioriser les tâches',
+    'Intervention du COP sur un projet',
+];
+
+// Store listitems
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// Insert list items into DOM
+function createList() {
+    [...taskDone]
+        .map(a => ({ value: a, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(a => a.value)
+        .forEach((person, index) => {
+            const listItem = document.createElement('li');
+
+            listItem.setAttribute('data-index', index);
+
+            listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="person-name">${person}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+      `;
+
+            listItems.push(listItem);
+
+            draggable_list.appendChild(listItem);
+        });
+
+    addEventListeners();
+}
+
+function dragStart() {
+    // console.log('Event: ', 'dragstart');
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+
+function dragEnter() {
+    // console.log('Event: ', 'dragenter');
+    this.classList.add('over');
+}
+
+function dragLeave() {
+    // console.log('Event: ', 'dragleave');
+    this.classList.remove('over');
+}
+
+function dragOver(e) {
+    // console.log('Event: ', 'dragover');
+    e.preventDefault();
+}
+
+function dragDrop() {
+    // console.log('Event: ', 'drop');
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+
+    this.classList.remove('over');
+}
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
+}
+
+// Check the order of list items
+function checkOrder() {
+    listItems.forEach((listItem, index) => {
+        const taskName = listItem.querySelector('.draggable').innerText.trim();
+
+        if (taskName !== taskDone[index]) {
+            listItem.classList.add('wrong');
+            alert("Dommage tu n'as pas mémorisé, il faut donc recommencer.")
+            window.location.href = "../../index.php";
+            index = numOflimit+1;
+        } else {
+            listItem.classList.remove('wrong');
+            listItem.classList.add('right');
+            window.confirm("Bravo ! \nTu as validé la compétence suivante : Prendre en compte les retours d’expérience des grands types de projet.");
+            window.location.href = "/assets/page/7.php";
+            index = numOflimit+1;
+        }
+    });
+}
+
+function addEventListeners() {
+    const draggables = document.querySelectorAll('.draggable');
+    const dragListItems = document.querySelectorAll('.draggable-list li');
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', dragStart);
+    });
+
+    dragListItems.forEach(item => {
+        item.addEventListener('dragover', dragOver);
+        item.addEventListener('drop', dragDrop);
+        item.addEventListener('dragenter', dragEnter);
+        item.addEventListener('dragleave', dragLeave);
+    });
+}
+
+check.addEventListener('click', checkOrder);
+
+const arnaud = document.getElementById("arnaud");
+const popup = document.getElementById("popup-indice");
+
+arnaud.addEventListener("click", ShowPopup);
+function ShowPopup() {
+    if (popup.style.opacity == "0") {
+        popup.style.opacity = "1";
+    } else {
+        popup.style.opacity = "0";
+    }
+}
+
---
+++ assets/page/6.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Enigme 6</title>
+    <link rel="icon" type="image/x-icon" href="/assets/img/logo.png">
+    <link rel="stylesheet" href="/assets/css/6.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/3da1a747b2.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div id="popup-indice">
+        Les inspecteurs sont connus pour trouver les petits détails
+    </div>
+    <header>
+        <div class="logo">
+            <img onclick="location.href='../../index.php'" style="width: 100%;" src="/assets/img/logo.png" alt="">
+        </div>
+        <div class="number-current color-indice">
+            <p>6</p>
+        </div>
+    </header>
+    <div class="container">
+        <div class="row">
+            <h2>“En tant que COP, tu te dois d’apprendre de tes erreurs et revenir sur tes expériences. Pour ce premier
+                jour, on te demande de <g>revenir sur ton expérience aujourd’hui</g> car en effet, ne pas apprendre de
+                ses
+                erreurs
+                veut dire
+                les faire à nouveau.”</h2>
+        </div>
+    </div>
+    <div class="section-screen">
+        <div class="row"> 
+            <div class="border-screen">
+                <div class="text-normal">Expériences acquises aujourd'hui</div>
+                <ul class="draggable-list" id="draggable-list"></ul>
+                <div class="center">
+                <button class="check-btn" id="check">
+                    Valider
+                </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <btn class="indice-2" id="arnaud">
+        <img style="width: 100%" src="/assets/img/arnaud-pensif.png" alt="">
+    </btn>
+    <script src="/assets/js/6.js"></script>
+</body>
+
+</html>
