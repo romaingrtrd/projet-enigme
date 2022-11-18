57218b63b86ab874fb0cd2ad43b722a4b3bc9a90
lokmane-elkanboui <lokmane.elkanboui@gmail.com>
2022-11-18 08:37:39 +0100

    index

--- index.php
+++ index.php
@@ -6,9 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./assets/css/index.css" rel="stylesheet">
     <link href="/dist/output.css" rel="stylesheet">
-    <title>Accueil</title>
+    <title>Index</title>
 </head>
 <body>
-<p>TEST</p>
+    <div class="navbar">
+        <button>  <img src="/assets/img/logo.png">  </button>
+        <a href="1.php">
+        <button>  <img src="/assets/img/button.png">  </button>
+        </a>
+    </div>
+    <section class="accueil">
+        <div class="contener-accueil">
+        <h1 class="title">ENI<font color="white">G</font>M<font color="white">4</font>, vivez l'aventure<br> d'Arnaud <font color="white">Stickman</font></h1>
+        </div>
+        <a href="1.php">
+        <div class="img"><button>
+            <img src="/assets/img/jouer.png" alt="">
+            <img src="/assets/img/jouer-hover.png" class="hover_img">
+        </button></div>
+        </a>
+</section>
 </body>
 </html>
